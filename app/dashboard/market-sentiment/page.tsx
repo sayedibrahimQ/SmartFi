3065a04e1e541abb6318903603660a67e159d879
Sayed <sayedibrahim.co@gmail.com>
2025-02-28 01:55:08 +0200

    m

--- app/dashboard/market-sentiment/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardShell } from "@/components/dashboard-shell";
-
-export default function market() {
-  return (
-    <DashboardShell>
-    Recommendations
-    </DashboardShell>
-  );
-}
